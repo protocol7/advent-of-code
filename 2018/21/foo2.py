2c57abf2ca9b2a92ad566cc170ec5271981ebe7c
Niklas Gustavsson <niklas@protocol7.com>
2018-12-21 08:37:51 +0100

    21 part 2

---
+++ 2018/21/foo2.py
@@ -0,0 +1,34 @@
+r3, r4 = 0, 0
+
+r4 = 65536
+r3 = 7041048
+
+seen = set()
+last_seen = None
+while True:
+    #bani 4 255 5
+    x = r4 & 255
+
+    # addr 3 5 3
+    r3 = r3 + x
+
+    # bani 3 16777215 3
+    r3 = r3 & 16777215
+
+    #muli 3 65899 3
+    r3 = r3 * 65899
+
+    # bani 3 16777215 3
+    r3 = r3 & 16777215
+
+    # gtir 256 4 5
+    if 256 > r4:
+        if r3 in seen:
+            print(last_seen)
+            break
+        seen.add(r3)
+        last_seen = r3
+        r4 = r3 | 65536
+        r3 = 7041048
+    else:
+        r4 = r4 // 256
