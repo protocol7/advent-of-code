c101cfec1a407d4e4bfb6b073e32402973f0d4f1
Niklas Gustavsson <niklas@protocol7.com>
2018-12-27 00:10:44 +0100

    2015 16

---
+++ 2015/16/foo2.py
@@ -0,0 +1,42 @@
+import sys
+
+def parse(line):
+    l = line.split()
+    sue = int(l[1].strip(":"))
+    d = {}
+    for i in range(2, len(l), 2):
+        t, data = l[i].strip(":"), int(l[i+1].strip(","))
+        d[t] = data
+    return sue, d
+
+pattern = {
+        "children": 3,
+        "cats": 7,
+        "samoyeds": 2,
+        "pomeranians": 3,
+        "akitas": 0,
+        "vizslas": 0,
+        "goldfish": 5,
+        "trees": 3,
+        "cars": 2,
+        "perfumes": 1}
+
+parsed = {k:v for k, v in map(parse, sys.stdin)}
+
+for sue, data in parsed.iteritems():
+    match = True
+    for k, v in pattern.iteritems():
+        if k in data:
+            if (k == "trees" or k == "cats"):
+                if data[k] <= v:
+                    match = False
+                    break
+            elif (k == "pomeranians" or k == "goldfish"):
+                if data[k] >= v:
+                    match = False
+                    break
+            elif data[k] != v:
+                match = False
+                break
+    if match:
+        print(sue)
