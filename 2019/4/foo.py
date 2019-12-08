064afea7b74c76e3b63430cb2a926949a68f1a9e
Niklas Gustavsson <niklas@protocol7.com>
2019-12-08 20:32:36 +0100

    Day 4

---
+++ 2019/4/foo.py
@@ -0,0 +1,10 @@
+from collections import *
+
+start = 264360
+end = 746325
+
+def f(i):
+    s = str(i)
+    return list(s) == sorted(s) and filter(lambda x: x >= 2, Counter(s).values())
+
+print(len(filter(f, range(start, end + 1))))
