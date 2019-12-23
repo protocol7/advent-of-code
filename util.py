b1ce31783092e148ed4f203f257fc5e21361af87
Niklas Gustavsson <niklas@protocol7.com>
2019-12-23 21:30:23 +0100

    Add Memoize

--- util.py
+++ util.py
@@ -205,3 +205,15 @@ def binary_search(lo, hi, check):
             hi = x
         else:
             lo = x
+
+# Simply memoize implementation, use with @Memoize
+class Memoize:
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+    def __call__(self, *args):
+        if not args in self.memo:
+            self.memo[args] = self.f(*args)
+        #Warning: You may wish to do a deepcopy here if returning objects
+        return self.memo[args]
+
